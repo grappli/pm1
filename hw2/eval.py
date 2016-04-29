b5fce4d36bb4b87992ab70e30c71a2a43c2bce12
Steffen Castle <steffen.castle@gmail.com>
2016-04-29 12:16:12 +0200

    Helper methods

--- hw2/eval.py
+++ hw2/eval.py
@@ -11,9 +11,12 @@ def find_closest_word(word_vec, vecs):
     x = find_closest(word_vec, vecs)
     return dataset.word_dict.keys()[dataset.word_dict.values().index(x)]
 
+def get_vec(word):
+    return vec[dataset.word_dict[word]]
+
 filename = 'proj_10.npy'
 
 dataset = CorpusDataset(0)
 
 vec = np.load('C:\Users\Steffen\PycharmProjects\pm1\hw2\proj_10.npy')
-print find_closest_word(vec[dataset.word_dict['Paris']] - vec[dataset.word_dict['interesting']] + vec[dataset.word_dict['elephant']], vec)
+print find_closest_word(get_vec('Paris') - get_vec('interesting') + get_vec('elephant'), vec)
